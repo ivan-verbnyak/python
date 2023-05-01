c273522951bbec7c0407f624b8294d7e178fd334
ivan-verbnyak <kadet717@ya.ru>
2023-05-01 22:35:18 +0500

    dz5task2

---
+++ DZ/DZ_6/task2.py
@@ -0,0 +1,14 @@
+# Определить индексы элементов массива (списка), значения которых
+# принадлежат заданному диапазону (т.е. не меньше заданного минимума и не
+# больше заданного максимума)
+
+import random
+
+
+
+list_1 = [random.randint(1,5) for _ in range(10)]
+min_number = int(input())
+max_number = int(input())
+for i in range(len(list_1)):
+    if min_number <= list_1[i] <= max_number:
+        print(i)
